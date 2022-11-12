70c441cf0056c700e717c85a28a225ce7fd0c7c9
hcooch2ch3 <hcooch2ch3@gmail.com>
2022-11-13 03:50:53 +0900

    AppStorePreviewViewModel에 뷰에서 필요한 데이터를 프로퍼티로 추가 - 뷰에서 필요한 데이터를 프로퍼티에 추가하고, 데이터를 초기화 하도록 추가

--- Befamily-Messenger-Description-App/AppStorePreviewViewModel.swift
+++ Befamily-Messenger-Description-App/AppStorePreviewViewModel.swift
@@ -13,6 +13,21 @@ class AppStorePreviewViewModel: ObservableObject {
     private var cancellableSet: Set<AnyCancellable> = []
     
     @Published var appStorePreviews: AppStorePreviews?
+    @Published var iconImageURL: URL?
+    @Published var title: String = "앱 이름"
+    @Published var sellerName: String = "판매자명"
+    @Published var userRatingCount: Int = 0
+    @Published var averageUserRating: String = "0.0"
+    @Published var minimumUserAge: String = "0+"
+    @Published var category: String = "카테고리"
+    @Published var developerName: String = "개발자"
+    @Published var language: String = "언어"
+    @Published var moreLanguageCount: Int = 0
+    @Published var version: String = "0.0.0"
+    @Published var versionDate: String = "0일 전"
+    @Published var versionDescription: String = "버전 상세 설명"
+    @Published var appDescription: String = "앱 상세 설명"
+    @Published var previewImageURLs: [URL] = []
     
     init() {
         getAppStorePreviews()
@@ -24,8 +39,31 @@ class AppStorePreviewViewModel: ObservableObject {
                 debugPrint(dataResponse.error!)
             } else {
                 self.appStorePreviews = dataResponse.value
+                self.setProperties()
                 print(self.appStorePreviews)
             }
         }.store(in: &cancellableSet)
+    }
+    
+    private func setProperties() {
+        guard let appStorePreviews = self.appStorePreviews,
+              appStorePreviews.resultCount > 0,
+              let appStorePreview = appStorePreviews.results.first else { return }
+        
+        self.title = appStorePreview.trackName
+        self.sellerName = appStorePreview.sellerName
+        self.userRatingCount = appStorePreview.userRatingCount
+        self.averageUserRating = String(format: "%.1f", appStorePreview.averageUserRating)
+        self.minimumUserAge = appStorePreview.contentAdvisoryRating
+        self.category = appStorePreview.genres.first ?? "알 수 없음"
+        self.developerName = appStorePreview.artistName
+        self.language = appStorePreview.languageCodesISO2A.first ?? "언어"
+        self.moreLanguageCount = appStorePreview.languageCodesISO2A.count - 1
+        self.version = appStorePreview.version
+        self.versionDate = appStorePreview.currentVersionReleaseDate.relativeDateTimeString
+        self.versionDescription = appStorePreview.releaseNotes
+        self.appDescription = appStorePreview.resultDescription
+        self.iconImageURL = URL(string: appStorePreview.artworkUrl100)
+        self.previewImageURLs = appStorePreview.screenshotUrls.compactMap { URL(string: $0) }
     }
 }
