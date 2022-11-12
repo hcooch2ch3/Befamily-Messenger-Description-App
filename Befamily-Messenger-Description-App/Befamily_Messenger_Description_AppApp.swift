de62853aabcf451c256daf52489c8090aab3c66f
hcooch2ch3 <hcooch2ch3@gmail.com>
2022-11-12 13:47:26 +0900

    프로젝트 추가

---
+++ Befamily-Messenger-Description-App/Befamily_Messenger_Description_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Befamily_Messenger_Description_AppApp.swift
+//  Befamily-Messenger-Description-App
+//
+//  Created by 임성민 on 2022/11/11.
+//
+
+import SwiftUI
+
+@main
+struct Befamily_Messenger_Description_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
